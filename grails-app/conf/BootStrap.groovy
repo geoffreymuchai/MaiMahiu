c75f32d473242d0e74269c30762e692cf1f96239
Trevor Kimenye <trevor@kimenye.com>
2011-10-22 15:13:34 +0300

    tags for complaint types

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -10,15 +10,15 @@ class BootStrap {
     }
 
     def createComplaintTypes() {
-        def wq = ComplaintType.findByDescription("Water Quality") ?: new ComplaintType(description: "Water Quality", menuIdx: 0).save()
-        def dq = ComplaintType.findByDescription("Disconnection") ?: new ComplaintType(description: "Disconnection", menuIdx: 1).save()
-        def lk = ComplaintType.findByDescription("Leakages") ?: new ComplaintType(description: "Leakages", menuIdx: 2).save()
-        def cr = ComplaintType.findByDescription("Corruption") ?: new ComplaintType(description: "Corruption", menuIdx: 3).save()
-        def wb = ComplaintType.findByDescription("Wrong Billing") ?: new ComplaintType(description: "Wrong Billing", menuIdx: 4).save()
-        def fm = ComplaintType.findByDescription("Faulty Meter") ?: new ComplaintType(description: "Faulty Meter", menuIdx: 5).save()
-        def dp = ComplaintType.findByDescription("Delay in new application") ?: new ComplaintType(description: "Delay in new application", menuIdx: 6).save()
-        def sb = ComplaintType.findByDescription("Sewer Bursts") ?: new ComplaintType(description: "Sewer Bursts", menuIdx: 7).save()
-        def ot = ComplaintType.findByDescription("Other") ?: new ComplaintType(description: "Other", menuIdx: 8).save()
+        def wq = ComplaintType.findByDescription("Water Quality") ?: new ComplaintType(description: "Water Quality", menuIdx: 0, urgencyRating: 1, tags: "Dirty,Unclean").save()
+        def dq = ComplaintType.findByDescription("Disconnection") ?: new ComplaintType(description: "Disconnection", menuIdx: 1, urgencyRating: 2, tags: "Disconnected").save()
+        def lk = ComplaintType.findByDescription("Leakages") ?: new ComplaintType(description: "Leakages", menuIdx: 2, urgencyRating: 3, tags: "Leakages,Leak").save()
+        def cr = ComplaintType.findByDescription("Corruption") ?: new ComplaintType(description: "Corruption", menuIdx: 3, urgencyRating: 4, tags: "Corruption").save()
+        def wb = ComplaintType.findByDescription("Wrong Billing") ?: new ComplaintType(description: "Wrong Billing", menuIdx: 4, urgencyRating: 5, tags: "Bill,Money,Overcharged").save()
+        def fm = ComplaintType.findByDescription("Faulty Meter") ?: new ComplaintType(description: "Faulty Meter", menuIdx: 5, urgencyRating: 6, tags: "Meter").save()
+        def dp = ComplaintType.findByDescription("Delay in new application") ?: new ComplaintType(description: "Delay in new application", menuIdx: 6, urgencyRating: 7, tags: "Delay,application,new").save()
+        def sb = ComplaintType.findByDescription("Sewer Bursts") ?: new ComplaintType(description: "Sewer Bursts", menuIdx: 7, urgencyRating: 1, "Sewer,Bursts").save()
+        def ot = ComplaintType.findByDescription("Other") ?: new ComplaintType(description: "Other", menuIdx: 8, urgencyRating: 9).save()
 
     }
 
--- grails-app/domain/complaintsystem/ComplaintType.groovy
+++ grails-app/domain/complaintsystem/ComplaintType.groovy
@@ -7,6 +7,8 @@ class ComplaintType {
 
     String description
     int menuIdx
+    int urgencyRating //rating for the urgency of an issue
+    String tags //a comma sepearted list of tags that can match this issue
 
     String toString() {
         return "${description}"
