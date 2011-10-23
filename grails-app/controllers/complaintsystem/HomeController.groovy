28c80cdd4d672448bc7a0e586c6653e2dbc7bf1b
Trevor Kimenye <trevor@kimenye.com>
2011-10-23 09:17:04 +0300

    Added security service

--- grails-app/controllers/complaintsystem/HomeController.groovy
+++ grails-app/controllers/complaintsystem/HomeController.groovy
@@ -5,19 +5,24 @@ import org.apache.shiro.SecurityUtils
 
 class HomeController {
 
-    def index = {
-        def currentUser = SecurityUtils.subject
-        def primaryPrincipal = currentUser.principal.toString()
+    def securityService
 
-        def user = ShiroUser.findByUsername(primaryPrincipal)
-		if (user) {
-			def isWasreb = false
-	//        if (user.roles.)
-			user.roles.each {
-				if (it.name == "WASREB") {
-				   isWasreb = true
-				}
-			}
+    def index = {
+//        def currentUser = SecurityUtils.subject
+//        def primaryPrincipal = currentUser.principal.toString()
+
+//        def user = ShiroUser.findByUsername(primaryPrincipal)
+//		if (user) {
+        if (securityService.isLoggedIn())  {
+//			def isWasreb = false
+//	//        if (user.roles.)
+//			user.roles.each {
+//				if (it.name == "WASREB") {
+//				   isWasreb = true
+//				}
+//			}
+            def user = securityService.getLoggedInUser()
+            def isWasreb = securityService.isWasrebUser()
 
 			if (!isWasreb) {
 				def complaints = Complaint.findAllByUtility(user.utility)
---
+++ grails-app/services/complaintsystem/SecurityService.groovy
@@ -0,0 +1,38 @@
+package complaintsystem
+
+import org.apache.shiro.SecurityUtils
+
+class SecurityService {
+
+    static transactional = true
+
+    boolean isWasrebUser() {
+        if (isLoggedIn()) {
+            def user = getLoggedInUser()
+			def isWasreb = false
+			user.roles.each {
+				if (it.name == "WASREB") {
+				   isWasreb = true
+				}
+			}
+            return isWasreb
+        }
+        return false
+    }
+
+    ShiroUser getLoggedInUser() {
+        def currentUser = SecurityUtils.subject
+        def primaryPrincipal = currentUser.principal.toString()
+
+        def user = ShiroUser.findByUsername(primaryPrincipal)
+    }
+
+    boolean isLoggedIn() {
+        def currentUser = SecurityUtils.subject
+        def primaryPrincipal = currentUser.principal.toString()
+
+        def user = ShiroUser.findByUsername(primaryPrincipal)
+
+        return user != null
+    }
+}
