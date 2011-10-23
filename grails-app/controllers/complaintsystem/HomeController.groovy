df8b0ad7e4049efd4a87ae040e3588231832252f
Trevor Kimenye <trevor@kimenye.com>
2011-10-23 09:17:20 +0300

    Changed home controller

--- grails-app/controllers/complaintsystem/HomeController.groovy
+++ grails-app/controllers/complaintsystem/HomeController.groovy
@@ -8,19 +8,7 @@ class HomeController {
     def securityService
 
     def index = {
-//        def currentUser = SecurityUtils.subject
-//        def primaryPrincipal = currentUser.principal.toString()
-
-//        def user = ShiroUser.findByUsername(primaryPrincipal)
-//		if (user) {
         if (securityService.isLoggedIn())  {
-//			def isWasreb = false
-//	//        if (user.roles.)
-//			user.roles.each {
-//				if (it.name == "WASREB") {
-//				   isWasreb = true
-//				}
-//			}
             def user = securityService.getLoggedInUser()
             def isWasreb = securityService.isWasrebUser()
 
