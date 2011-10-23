7054a8c2d2e10dc91b3c18c24087e7e5f765b4dc
Geoffrey <geoffreymuchai@gmail.com>
2011-10-23 12:44:58 +0300

    Added automatic response

--- grails-app/controllers/complaintsystem/ComplaintController.groovy
+++ grails-app/controllers/complaintsystem/ComplaintController.groovy
@@ -11,6 +11,7 @@ class ComplaintController {
 		complaint.dateResolved = new Date()
 		complaint.resolvedBy = securityService.getLoggedInUser()
 		complaint.save(flush:true)
+		def response = new Response(phoneNumber: complaint.affects.phoneNumber, message: "Your earlier complaint (reference  number ${complaint.ticketNumber}) has been resolved. If the problem still persists, resend your complaint").save(flush: true, failOnError: true)
 		if(securityService.isWasrebUser()) {
 			redirect controller: "home", action: "wasreb"
 		} else {
